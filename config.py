5c4224fc37a85eadb3aa6d00e7779d77891db5fd
jculley01 <113144839+jculley01@users.noreply.github.com>
2024-02-09 08:46:45 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-SQLALCHEMY_DATABASE_URI = 'postgresql://jculley:931012jC@localhost/textstorage'
+SQLALCHEMY_DATABASE_URI = 'enter here'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = 'secret_key'
 MAX_CONTENT_LENGTH = 16 * 1024 * 1024  # 16 MB limit
